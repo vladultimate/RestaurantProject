83996aed3fd5d0613cb0afc84b567742adcbcd9e
vladultimate <vladultimate2010@gmail.com>
2025-08-01 18:47:09 +0300

    added model cart

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,17 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
+
 class Dish(models.Model):
     price = models.IntegerField()
     name = models.CharField(max_length=50)
     category = models.CharField(max_length=50)
     description = models.TextField()
     
+
 class Feedback(models.Model):
     rating = models.IntegerField()
     description = models.CharField(max_length=250)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+
 
 class Profile(models.Model):
     ROLE_CHOICES = [
@@ -20,3 +22,8 @@ class Profile(models.Model):
     ]
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='user')
+
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    dishes = models.ManyToManyField(Dish) 
